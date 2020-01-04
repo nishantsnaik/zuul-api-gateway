a4e0bf0654fbec7f14c7041e381c8a407ff95621
Nishant Naik <nishantsnaik@gmail.com>
2020-01-04 17:40:52 -0600

    first commit

---
+++ src/main/java/com/nishant/zuulapigateway/ZuulApiGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.nishant.zuulapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class ZuulApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/nishant/zuulapigateway/ZuulLoggingFilter.java
@@ -0,0 +1,41 @@
+package com.nishant.zuulapigateway;
+
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Service
+public class ZuulLoggingFilter extends ZuulFilter {
+
+    private Logger LOGGER = LoggerFactory.getLogger(ZuulLoggingFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+
+        HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+
+        LOGGER.info("request -> {} request uri -> {}",  request, request.getRequestURI() );
+        return null;
+    }
+}
